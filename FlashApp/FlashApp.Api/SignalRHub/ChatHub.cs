d78b6f1c403cceed3e9abd08122e8934370b55a4
botrkl <96987652+botrkl@users.noreply.github.com>
2023-10-23 23:20:43 +0300

    Implement realtime communication, fixed a bug in sorting messages by sending time

---
+++ FlashApp/FlashApp.Api/SignalRHub/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FlashApp.Api.SignalRHub
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            //save in db
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
